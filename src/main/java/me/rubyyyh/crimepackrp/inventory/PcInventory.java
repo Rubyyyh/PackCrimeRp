2f77653e38066b2da28fa047b6f05e2637294e35
Rubyyyh <Roby200737f@icloud.com>
2024-07-08 00:29:25 +0200

    CrimePack

---
+++ src/main/java/me/rubyyyh/crimepackrp/inventory/PcInventory.java
@@ -0,0 +1,20 @@
+package me.rubyyyh.crimepackrp.inventory;
+
+import fr.minuskube.inv.content.InventoryContents;
+import fr.minuskube.inv.content.InventoryProvider;
+import net.wesjd.anvilgui.AnvilGUI;
+import org.bukkit.entity.Player;
+
+public class PcInventory implements InventoryProvider {
+
+
+    @Override
+    public void init(Player player, InventoryContents inventoryContents) {
+        
+    }
+
+    @Override
+    public void update(Player player, InventoryContents inventoryContents) {
+
+    }
+}
